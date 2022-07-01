35c7933a8f056f1d1301935a8738bc4475fb03cc
domen.golob <domen.golob@outfit7.com>
2022-07-01 19:02:24 +0200

    Add rotation behaviour

---
+++ KarticeWebApp/Pages/Utils.cs
@@ -0,0 +1,8 @@
+﻿namespace KarticeWebApp.Pages;
+
+public class Utils {
+    public static void RotateImage(ref int rotation, out string imageStyle) {
+        rotation = rotation == 0 ? 90 : 0;
+        imageStyle = $"transform: rotate({rotation}deg) translateX({(rotation == 90 ? 30 : 0)}%);";
+    }
+}
