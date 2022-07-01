eadde59c9575614d8d90dd1b76237fc834601061
domen.golob <domen.golob@outfit7.com>
2022-07-01 18:34:27 +0200

    forgoten files

---
+++ KarticeWebApp/Data/Links.cs
@@ -0,0 +1,7 @@
+﻿namespace KarticeWebApp.Data;
+
+public static class Links {
+    public const string Home = "Home";
+    public const string Spar = nameof(Spar);
+    public const string Omw = nameof(Omw);
+}
